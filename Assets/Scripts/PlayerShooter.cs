a45083a0e1cf67f0da9dc19b25c15cd5fe68bb76
User <user@DESKTOP-EDS44JG>
2024-12-19 13:02:34 +0900

    15.5 Done

--- Assets/Scripts/PlayerShooter.cs
+++ Assets/Scripts/PlayerShooter.cs
@@ -29,19 +29,40 @@ public class PlayerShooter : MonoBehaviour {
 
     private void Update() {
         // 입력을 감지하고 총 발사하거나 재장전
+        if(playerInput != null) {  
+            if(playerInput.fire) {
+                gun.Fire();
+            }
+            else if(playerInput.reload) {
+                if(gun.Reload()) {
+                    playerAnimator.SetTrigger("Reload");
+                }
+            }
+        }
+        UpdateUI(); // 남은 탄약 UI 갱신
     }
 
-    // 탄약 UI 갱신
+    // 남은 탄약 UI 갱신
     private void UpdateUI() {
         if (gun != null && UIManager.instance != null)
         {
-            // UI 매니저의 탄약 텍스트에 탄창의 탄약과 남은 전체 탄약을 표시
-            UIManager.instance.UpdateAmmoText(gun.magAmmo, gun.ammoRemain);
+            UIManager.instance.UpdateAmmoText(gun.magAmmo, gun.ammoRemain); // UI 매니저의 탄약 텍스트에 탄창의 탄약과 남은 전체 탄약을 표시
         }
     }
 
     // 애니메이터의 IK 갱신
     private void OnAnimatorIK(int layerIndex) {
-        
+        gunPivot.position = playerAnimator.GetIKHintPosition(AvatarIKHint.RightElbow); // 총의 기준점을 오른쪽 팔꿈치 위치로
+
+        playerAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1.0f); // 왼손 위치를 지정
+        playerAnimator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1.0f); // 왼손 회전을 지정
+        playerAnimator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandMount.position); // 왼손의 위치를 왼손잡이의 위치로
+        playerAnimator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandMount.rotation); // 왼손의 회전을 왼손잡이의 회전으로
+
+        playerAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1.0f); // 오른손 위치를 지정
+        playerAnimator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1.0f); // 오른손 회전을 지정
+        playerAnimator.SetIKPosition(AvatarIKGoal.RightHand, rightHandMount.position); // 오른손의 위치를 오른손잡이의 위치로
+        playerAnimator.SetIKRotation(AvatarIKGoal.RightHand, rightHandMount.rotation); // 오른손의 회전을 오른손잡이의 회전으로
+
     }
 }
